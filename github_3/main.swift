dd1b3a5dff45e7dfa47742f15f7f65c9cd43b0e4
john Doe <jeffreyalmonte@Pursuits-MacBook-Pro-10.local>
2018-10-05 21:52:42 -0400

    third try

---
+++ github_3/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  github_3
+//
+//  Created by Jeffrey Almonte on 10/5/18.
+//  Copyright © 2018 Jeffrey Almonte. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//third try
